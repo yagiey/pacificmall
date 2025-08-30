05229de108c67715bf26cb633de7addd0ff56f2e
Yagiey <yagiey@gmail.com>
2025-08-30 12:29:19 +0900

    after 2-3-3

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function my_enqueue_scripts() {
+    $uri = esc_url( get_template_directory_uri() );
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bundle_js', $uri . '/assets/js/bundle.js', [] );
+    wp_enqueue_style( 'my_styles', $uri . '/assets/css/styles.css', [] );
+}
+add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts' );
