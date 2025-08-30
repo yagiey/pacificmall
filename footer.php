bb9e5f4eced0e27828121ba42087aba045fef576
Yagiey <yagiey@gmail.com>
2025-08-30 10:57:24 +0900

    after 2-3-1

---
+++ footer.php
@@ -0,0 +1,52 @@
+    <footer class="footer" id="footer">
+      <div class="footerContents">
+        <div class="footerContents-contact">
+          <div class="enterprise-logo">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/svg/logo-footer.svg" alt="PACIFIC MALL DEVELOPMENT" />
+          </div>
+          <div class="enterprise-detail">
+            <p class="name">パシフィックモール開発株式会社</p>
+            <p class="address">
+              東京都千代田区大手町0-1-2<br />
+              パシフィックモールビルディング18F 
+            </p>
+          </div>
+        </div>
+        <div class="footerContents-sitemap">
+          <nav class="footer-nav">
+            <ul class="menu">
+              <li class="menu-item">
+                <a class="nav-link" href="#">企業情報</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">会社概要</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">事業紹介</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">沿革</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">店舗情報</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">地域貢献活動</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">ニュースリリース</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">お問い合わせ</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <p class="copyright">
+        <small class="copyright-text">&#169; 2023 PACIFIC MALL DEVELOPMENT CO., LTD.</small>
+      </p>
+    </footer>
+  </div><!-- /.container -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="keywords" content="共通キーワード" />
+  <meta name="description" content="共通ディスクリプション" />
+  <title>PACIFIC MALL DEVELOPMENT</title>
+  <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/favicon.ico" />
+  <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/css/styles.css" />
+  <script type="text/javascript" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/js/bundle.js"></script>
+</head>
+<body>
+  <div class="container">
+    <header id="header">
+      <div class="header-inner">
+        <div class="logo">
+          <a class="logo-header" href="/pacificmall">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-main.svg" class="main-logo" alt="PACIFIC MALL DEVELOPMENT" />
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/common/logo-fixed.svg" class="fixed-logo" alt="PACIFIC MALL DEVELOPMENT" />
+          </a>
+        </div>
+        <button class="toggle-menu js-toggoleNav">
+          <span class="toggle-line">メニュー</span>
+        </button>
+        <div class="header-nav">
+          <nav class="global-nav">
+            <ul class="menu">
+              <li class="menu-item">
+                <a class="nav-link active" href="#">ホーム</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">企業情報</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">店舗情報</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">地域貢献活動</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">ニュースリリース</a>
+              </li>
+              <li class="menu-item">
+                <a class="nav-link" href="#">お問い合わせ</a>
+              </li>
+            </ul>
+          </nav>
+          <form class="search-form" role="search" method="get" action="">
+            <div class="search-box">
+              <input type="text" class="search-input" name="" placeholder="キーワードを入力してください" />
+              <button type="submit" class="button-submit"></button>
+            </div>
+            <div class="search-buttons">
+              <button type="button" class="close-icon js-searchIcon"></button>
+              <button type="button" class="search-icon js-searchIcon"></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </header>
+    <section class="section-contents" id="keyvisual">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/bg-section-keyvisual.jpg" alt="MAIN IMAGE" />
+      <div class="wrapper">
+        <h1 class="site-title">Connecting the future.</h1>
+        <p class="site-caption">
+          私たちパシフィックモール開発は<br />
+          世界各地のショッピングモール開発を通じて<br />
+          人と人、人と地域を結ぶお手伝いをしています。
+        </p>
+      </div>
+    </section>
