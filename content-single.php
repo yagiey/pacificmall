df1f4278aa8a86a47a231d6190be17bc06e70858
Yagiey <yagiey@gmail.com>
2025-09-01 12:32:52 +0900

    after 4-5-4

---
+++ content-single.php
@@ -0,0 +1,13 @@
+                      <div class="news">
+                        <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
+                        <p class='title'><?php the_title(); ?></p>
+                        <div class="news-body"><?php the_content(); ?></div>
+                      </div>
+                      <div class="more-news">
+                        <div class="next">
+                          <a class="another-link" href="#">NEXT</a>
+                        </div>
+                        <div class="prev">
+                          <a class="another-link" href="#">PREV</a>
+                        </div>
+                      </div>
--- single.php
+++ single.php
@@ -5,22 +5,9 @@
                     <div class="main-wrapper">
 <?php
 if ( have_posts() ):
-  while ( have_posts() ) : the_post();
-?>
-                      <div class="news">
-                        <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
-                        <p class='title'><?php the_title(); ?></p>
-                        <div class="news-body"><?php the_content(); ?></div>
-                      </div>
-                      <div class="more-news">
-                        <div class="next">
-                          <a class="another-link" href="#">NEXT</a>
-                        </div>
-                        <div class="prev">
-                          <a class="another-link" href="#">PREV</a>
-                        </div>
-                      </div>
-<?php
+  while ( have_posts() ):
+    the_post();
+    get_template_part( 'content-single' );
   endwhile;
 endif;
 ?>
