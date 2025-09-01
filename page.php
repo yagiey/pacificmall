9e744888347717e5642df47b2b5b3d3105e22172
Yagiey <yagiey@gmail.com>
2025-09-01 10:28:36 +0900

    after 4-3-4

--- page.php
+++ page.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
-<div class="page-main">
-    <div class="lead-inner">
+    <div class="page-main">
+        <div class="lead-inner">
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ): the_post();
+        the_content();
+    endwhile;
+endif;
+?>
+        </div>
     </div>
-</div>
 <?php get_footer(); ?>
