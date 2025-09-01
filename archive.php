503ae4a9d9ef26cddef492d178cd644b7562aa33
Yagiey <yagiey@gmail.com>
2025-09-01 12:58:09 +0900

    after 4-5-5-1

---
+++ archive.php
@@ -0,0 +1,78 @@
+<?php get_header(); ?>
+              <div class="page-inner full-width">
+                <div class="page-main" id="pg-news">
+                  <div class="main-container">
+                    <div class="main-wrapper">
+                      <div class="newsLists">
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">インド 消費者向けアプリ配信サービス大手のメカ・インディアと業務提携開始</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">年末年始休業のお知らせ</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">米国ニューヨーク・パークアベニューモール 美術館 「The Art of Park Ave.」にて、特設イベント「世界の若手美術展」を開催</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">展示会ご来場のお礼</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">会社概要</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">地域貢献活動</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">ムンバイにタンドールモールがオープンしました</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">当社のホームページをリニューアルしました</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">「（仮称）マニラモール」計画概要を決定</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">人材募集のお知らせ (中途採用)</p>
+                          </div>
+                        </a>
+                        <a class="news-link" href="#">
+                          <div class="news-body">
+                            <time class="release">2021.01.01</time>
+                            <p class="title">インド支店を開設</p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+<?php get_footer();
