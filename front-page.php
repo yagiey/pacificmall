ee5ffcdda12d152183d146131b059ddbafb9a73d
Yagiey <yagiey@gmail.com>
2025-09-02 14:43:38 +0900

    after 4-8-1-1

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@ $shop_title = get_the_title();
 <?php wp_reset_postdata(); ?>
         <ul class="shops">
 <?php
-$shop_pages = get_child_pages( -1, $shop_obj->ID );
+$shop_pages = get_child_pages();
 if ( $shop_pages->have_posts() ) :
   while ( $shop_pages->have_posts() ) : $shop_pages->the_post();
 ?>
