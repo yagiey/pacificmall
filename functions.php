969609727ab8ba485a4e526863a51124fb8cf35c
Yagiey <yagiey@gmail.com>
2025-09-02 13:29:01 +0900

    after 4-7-1-1

--- functions.php
+++ functions.php
@@ -39,3 +39,24 @@ function get_child_pages( $number = -1 ) {
     $child_pages = new WP_Query( $args );
     return $child_pages;
 }
+
+// アイキャッチ画像を利用できるようにする
+add_theme_support( 'post-thumbnails' );
+
+// トップページのメイン画像用のサイズ設定
+add_image_size( 'top', 1077, 622, true );
+
+// 地域貢献活動一覧画像用のサイズ設定
+add_image_size( 'contribution', 557, 280, true );
+
+// トップページの地域貢献活動にて使用している画像用のサイズ設定
+add_image_size( 'front-contribution', 255, 189, true );
+
+// 企業情報・店舗一覧画像用のサイズ設定
+add_image_size( 'common', 465, 252, true );
+
+// 各ページのメイン画像用のサイズ設定
+add_image_size( 'detail', 1100, 330, true );
+
+// 検索一覧画像用のサイズ設定
+add_image_size( 'search', 168, 168, true );
