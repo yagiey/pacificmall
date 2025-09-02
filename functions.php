0407a623376d778c15392d7257badf7d25492311
Yagiey <yagiey@gmail.com>
2025-09-02 14:44:01 +0900

    after 4-8-1-2

--- functions.php
+++ functions.php
@@ -27,10 +27,14 @@ function get_main_title() {
 }
 
 // 子ページを取得する関数
-function get_child_pages( $number = -1 ) {
-    $parent_id = get_the_ID();
+function get_child_pages( $number = -1, $specified_id = null ) {
+    if ( isset( $specified_id ) ):
+        $parent_id = $specified_id;
+    else:
+        $parent_id = get_the_ID();
+    endif;
     $args = [
-        'posts_per_page' => -1,
+        'posts_per_page' => $number,
         'post_type' => 'page',
         'orderby' => 'menu_order',
         'order' => 'ASC',
