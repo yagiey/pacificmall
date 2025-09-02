bb59c741e9f36df30309aeec2c24e48048960fe2
Yagiey <yagiey@gmail.com>
2025-09-02 13:37:06 +0900

    after 4-7-2-8

--- content-common.php
+++ content-common.php
@@ -1,6 +1,6 @@
                     <li class="common-item">
                       <a class="common-link" href="<?php the_permalink(); ?>">
-                        <div class="common-image"><?php the_post_thumbnail(); ?></div>
+                        <div class="common-image"><?php the_post_thumbnail( 'common' ); ?></div>
                         <div class="common-body">
                           <p class="name"><?php the_title(); ?></p>
                           <p class="caption"><?php echo get_the_excerpt(); ?></p>
--- content-contribution.php
+++ content-contribution.php
@@ -1,6 +1,6 @@
 <article class="article-card">
   <a class="card-link" href="<?php the_permalink(); ?>">
-    <div class="image"><?php the_post_thumbnail(); ?></div>
+    <div class="image"><?php the_post_thumbnail( 'contribution' ); ?></div>
     <div class="body">
       <time><?php the_time( 'Y.m.d' ); ?></time>
       <p class="title"><?php the_title(); ?></p>
