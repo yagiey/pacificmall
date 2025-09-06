491786fa55ee0b5c0e915171324f8cfa88ccab30
Yagiey <yagiey@gmail.com>
2025-09-06 12:16:28 +0900

    after 5-6-3-2

--- content-common.php
+++ content-common.php
@@ -3,7 +3,7 @@
                         <div class="common-image"><?php the_post_thumbnail( 'common' ); ?></div>
                         <div class="common-body">
                           <p class="name"><?php the_title(); ?></p>
-                          <p class="caption"><?php echo get_the_excerpt(); ?></p>
+                          <p class="caption"><?php echo get_flexible_excerpt( 40 ); ?></p>
                           <div class="buttonBox">
                             <button type="button" class="seeDetail">MORE</button>
                           </div>
