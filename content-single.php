f77fff09f9cd948ed48cc1fd097f2af535e1bc08
Yagiey <yagiey@gmail.com>
2025-09-06 11:42:32 +0900

    after 5-5-1

--- content-single.php
+++ content-single.php
@@ -4,10 +4,20 @@
                         <div class="news-body"><?php the_content(); ?></div>
                       </div>
                       <div class="more-news">
+<?php
+$next_post = get_next_post();
+$prev_post = get_previous_post();
+if ( $next_post ):
+?>
                         <div class="next">
-                          <a class="another-link" href="#">NEXT</a>
+                          <a class="another-link" href="<?php echo get_permalink( $next_post->ID ); ?>">NEXT</a>
                         </div>
+<?php
+endif;
+if ( $prev_post ):
+?>
                         <div class="prev">
-                          <a class="another-link" href="#">PREV</a>
+                          <a class="another-link" href="<?php echo get_permalink( $prev_post->ID); ?>">PREV</a>
                         </div>
+<?php endif; ?>
                       </div>
