3c27ee7bbda12623130cbb95889dc76a6659d3cc
Yagiey <yagiey@gmail.com>
2025-09-08 14:39:17 +0900

    after 9-2-6-1

--- content-contribution.php
+++ content-contribution.php
@@ -1,10 +1,10 @@
 <article class="article-card">
-  <a class="card-link" href="<?php the_permalink(); ?>">
-    <div class="image"><?php the_post_thumbnail( 'contribution' ); ?></div>
+  <a class="card-link" href="<?php echo get_term_link( $term ); ?>">
+    <div class="image"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png"></div>
     <div class="body">
       <time><?php the_time( 'Y.m.d' ); ?></time>
-      <p class="title"><?php the_title(); ?></p>
-      <p class="excerpt"><?php echo get_the_excerpt(); ?></p>
+      <p class="title"><?php echo $term->name; ?></p>
+      <p class="excerpt"><?php echo $term->description; ?></p>
       <div class="buttonBox">
         <button type="button" class="seeDetail">MORE</button>
       </div>
