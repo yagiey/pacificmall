8521a002c5c1e2e38754da133ccccf3b65ca048d
Yagiey <yagiey@gmail.com>
2025-09-09 12:19:52 +0900

    after 9-3-2-13

--- header.php
+++ header.php
@@ -49,7 +49,7 @@ wp_nav_menu([
 
 <?php if ( is_front_page() ) : ?>
     <section class="section-contents" id="keyvisual">
-      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/bg-section-keyvisual.jpg" alt="MAIN IMAGE" />
+      <?php echo get_main_image(); ?>
       <div class="wrapper">
         <h1 class="site-title">Connecting the future.</h1>
         <p class="site-caption"><?php echo get_the_excerpt(); ?></p>
