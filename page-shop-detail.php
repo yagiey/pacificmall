38638dec45ba67d015b882ab2b326d4eb578ba8d
Yagiey <yagiey@gmail.com>
2025-09-09 14:24:18 +0900

    after 9-3-4-11

--- page-shop-detail.php
+++ page-shop-detail.php
@@ -24,63 +24,14 @@ endif;
                     <div class="shopList-inner">
                       <ul class="shopList">
 <?php
-if ( have_rows( 'first_shop_detail' ) ):
-  while ( have_rows( 'first_shop_detail' ) ): the_row();
-?>
-                      <li class="shopList-item">
-                          <div class="shop-image">
-<?php
-$image_id = get_sub_field( 'shop_img' );
-echo wp_get_attachment_image( $image_id, 'shop_detail' );
-?>
-                          </div>
-                          <div class="shop-body">
-                            <p class="shop-title"><?php the_sub_field( 'shop_name' ); ?></p>
-                            <p class="shop-caption"><?php the_sub_field( 'shop_strength' ); ?></p>
-                            <div class="shop-detail">
-                              <dl>
-                                <dt>営業時間</dt>
-                                <dd><?php the_sub_field( 'shop_hours' ); ?></dd>
-                              </dl>
-                              <dl>
-                                <dt>フロア情報</dt>
-                                <dd><?php the_sub_field( 'floor_info' ); ?></dd>
-                              </dl>
-                            </div>
-                          </div>
-                        </li>
-<?php
-  endwhile;
-endif;
-
-if ( have_rows( 'second_shop_detail' ) ):
-  while ( have_rows( 'second_shop_detail' ) ): the_row();
-?>
-                        <li class="shopList-item">
-                          <div class="shop-image">
-<?php
-$image_id = get_sub_field( 'shop_img' );
-echo wp_get_attachment_image( $image_id, 'shop_detail' );
-?>
-                          </div>
-                          <div class="shop-body">
-                            <p class="shop-title"><?php the_sub_field( 'shop_name' ); ?></p>
-                            <p class="shop-caption"><?php the_sub_field( 'shop_strength' ); ?></p>
-                            <div class="shop-detail">
-                              <dl>
-                                <dt>営業時間</dt>
-                                <dd><?php the_sub_field( 'shop_hours' ); ?></dd>
-                              </dl>
-                              <dl>
-                                <dt>フロア情報</dt>
-                                <dd><?php the_sub_field( 'floor_info' ); ?></dd>
-                              </dl>
-                            </div>
-                          </div>
-                        </li>
-<?php
-  endwhile;
-endif;
+$shops = [ 'first_shop_detail', 'second_shop_detail' ];
+foreach ( $shops as $shop ):
+  if ( have_rows( $shop ) ):
+    while ( have_rows( $shop ) ): the_row();
+      get_template_part( 'content-shop-detail' );
+    endwhile;
+  endif;
+endforeach;
 ?>
                       </ul>
                     </div>
